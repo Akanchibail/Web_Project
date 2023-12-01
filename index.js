0aee453e7c10afa77fb8ba924a8b074873985a7b
AkshayKanchibail <akanchibail@gmail.com>
2023-12-01 17:36:40 -0500

    server setup

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 app.use(express.json());
 
 const userRoutes = require("./server/routes/user");
+const noteRoutes = require("./server/routes/note");
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin","*");
@@ -14,9 +15,10 @@ app.use(function(req, res, next) {
 });
 
 app.use("/users", userRoutes);
+app.use("/notes", noteRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 
-console.log(PORT);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}!!!`));
---
+++ server/models/note.js
@@ -0,0 +1,15 @@
+const notes = [
+    {
+       content : "Hi",
+    },
+    {
+       content :"Hello",
+    },
+    {
+        content : "Good Morning",
+    },
+];
+
+let getNotes = () =>notes;
+
+module.exports = { getNotes };
